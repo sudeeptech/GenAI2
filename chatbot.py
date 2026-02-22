c49c00433ac62ecf89b4c9304b7de3d7cad66733
sudeeptech <94739504+sudeeptech@users.noreply.github.com>
2026-02-22 15:39:53 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -12,7 +12,7 @@ st.set_page_config(
     page_icon="🤖",
     layout="centered",
 )
-st.title("💬 Generative AI Chatbot")
+st.title("💬 Sudeeps AI Chatbot")
 
 # initiate chat history
 if "chat_history" not in st.session_state:
